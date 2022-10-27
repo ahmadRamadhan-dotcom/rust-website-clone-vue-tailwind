070cd1da4644a024a28537fc914a980914175502
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-27 13:09:41 +0700

    chore: responsive twitch drops for sm devices, md devices and lg devicesc

--- src/components/TwitchDrops.vue
+++ src/components/TwitchDrops.vue
@@ -1,10 +1,12 @@
 <template>
   <div
     id="twich-drops"
-    class="flex justify-center bg-[url('/twitch-bg.jpg')] bg-no-repeat bg-center bg-cover xl:p-[160px_160px]"
+    class="flex justify-center bg-[url('/twitch-bg.jpg')] sm:mt-14 bg-no-repeat bg-center bg-cover sm:p-[50px_50px] md:p-[130px_130px] lg:p-[160px_160px] md:-translate-y-20 lg:translate-y-0"
   >
     <div class="grid place-content-center place-items-center text-center">
-      <p class="font-['Bebas_Neue'] font-medium text-[#ffffff] text-[112px]">
+      <p
+        class="font-['Bebas_Neue'] font-medium text-[#ffffff] sm:text-[64px] text-[112px]"
+      >
         Twitch Drops
       </p>
       <p class="text-[#c8bbdc] text-base font-normal font-['Open_Sans']">
@@ -13,7 +15,7 @@
       <a
         href="https://twitch.facepunch.com/"
         target="_blank"
-        class="text-[#ffffff] font-['Bebas_Neue'] xl:mt-10 bg-[#cd412b] font-normal text-[28px] w-52"
+        class="text-[#ffffff] font-['Bebas_Neue'] sm:mt-10 xl:mt-10 bg-[#cd412b] font-normal sm:text-[24px] text-[28px] sm:w-40 sm:py-1 w-52"
         >get twitch drops</a
       >
     </div>
