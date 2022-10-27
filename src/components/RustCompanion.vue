413db4edcb6fa88c6662ac639c51b26b82625a73
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-27 15:50:32 +0700

    chore: responsive rust companion section for sm devices, md devices and lg devices

--- src/components/RustCompanion.vue
+++ src/components/RustCompanion.vue
@@ -1,20 +1,22 @@
 <template>
   <div
     id="rust-companion"
-    class="flex justify-center items-center xl:mt-10 xl:h-[600px] 2xl:h-[50vh]"
+    class="flex justify-center items-center sm:mt-14 xl:mt-10 xl:h-[600px] 2xl:h-[50vh]"
   >
-    <div class="xl:w-[1100px] xl:flex xl:items-center">
-      <div class="grid xl:h-[300px]">
+    <div
+      class="xl:w-[1160px] sm:flex sm:flex-col-reverse lg:flex lg:flex-row lg:items-center"
+    >
+      <div class="grid sm:p-[28px] lg:h-[280px] xl:h-[300px]">
         <h2
-          class="text-[#e4dad1] xl:leading-[60px] xl:w-[400px] font-bold font-['Bebas_Neue'] xl:text-[128px]"
+          class="text-[#e4dad1] sm:mt-14 sm:leading-[35px] lg:leading-[58px] xl:leading-[60px] sm:w-[200px] xl:w-[400px] font-bold font-['Bebas_Neue'] sm:text-[64px] lg:text-[128px]"
         >
           Rust+
-          <span class="text-[#bab1a8] font-[300] xl:text-[64px]"
+          <span class="text-[#bab1a8] font-[300] sm:text-[32px] lg:text-[64px]"
             >Companion</span
           >
         </h2>
         <p
-          class="font-['Open_Sans'] xl:leading-[30px] xl:mt-4 text-base text-[#bab1a8] font-medium xl:w-[550px]"
+          class="font-['Open_Sans'] xl:leading-[30px] sm:mt-4 xl:mt-4 text-base text-[#bab1a8] font-medium sm:w-full lg:w-[500px] xl:w-[550px]"
         >
           The official Rust companion app – stay connected to the game so you
           never miss an important moment again. Coordinate with your team,
@@ -23,7 +25,7 @@
         </p>
         <a
           href="https://rust.facepunch.com/companion"
-          class="text-xl text-center flex items-center justify-center xl:py-1 xl:w-[110px] xl:mt-6 text-[#ffffff] font-['Bebas_Neue'] bg-[#cd412b]"
+          class="text-xl text-center flex items-center justify-center sm:py-1.5 lg:py-1 sm:w-[100px] lg:w-[110px] sm:mt-4 xl:mt-6 text-[#ffffff] font-['Bebas_Neue'] bg-[#cd412b]"
           >learn more</a
         >
       </div>
