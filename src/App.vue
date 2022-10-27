a3a36fac0978425412f54806b94b020475931cdd
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-27 12:55:30 +0700

    chore: responsive twitch drops section for xl and 2xl devices

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <BannerHero />
   <Quote />
   <LatestNews />
+  <TwitchDrops />
 </template>
 
 <script setup>
@@ -10,4 +11,5 @@ import Header from "./components/Header.vue";
 import BannerHero from "./components/BannerHero.vue";
 import Quote from "./components/Quote.vue";
 import LatestNews from "./components/LatestNews.vue";
+import TwitchDrops from "./components/TwitchDrops.vue";
 </script>
---
+++ src/components/TwitchDrops.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    id="twich-drops"
+    class="flex justify-center bg-[url('/twitch-bg.jpg')] bg-no-repeat bg-center bg-cover xl:p-[160px_160px]"
+  >
+    <div class="grid place-content-center place-items-center text-center">
+      <p class="font-['Bebas_Neue'] font-medium text-[#ffffff] text-[112px]">
+        Twitch Drops
+      </p>
+      <p class="text-[#c8bbdc] text-base font-normal font-['Open_Sans']">
+        Monthly Twitch Drops from your favourite streamers.
+      </p>
+      <a
+        href="https://twitch.facepunch.com/"
+        target="_blank"
+        class="text-[#ffffff] font-['Bebas_Neue'] xl:mt-10 bg-[#cd412b] font-normal text-[28px] w-52"
+        >get twitch drops</a
+      >
+    </div>
+  </div>
+</template>
