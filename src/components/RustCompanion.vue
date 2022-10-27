60c2d99d875e47a6faec0c80face10a398f77076
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-27 15:54:45 +0700

    chore: responsive rust companion for mobile devices

--- src/components/RustCompanion.vue
+++ src/components/RustCompanion.vue
@@ -1,22 +1,23 @@
 <template>
   <div
     id="rust-companion"
-    class="flex justify-center items-center sm:mt-14 xl:mt-10 xl:h-[600px] 2xl:h-[50vh]"
+    class="flex justify-center items-center mt-20 sm:mt-14 xl:mt-10 xl:h-[600px] 2xl:h-[50vh]"
   >
     <div
-      class="xl:w-[1160px] sm:flex sm:flex-col-reverse lg:flex lg:flex-row lg:items-center"
+      class="xl:w-[1160px] flex flex-col-reverse sm:flex sm:flex-col-reverse lg:flex lg:flex-row lg:items-center"
     >
-      <div class="grid sm:p-[28px] lg:h-[280px] xl:h-[300px]">
+      <div class="grid p-[30px] sm:p-[28px] lg:h-[280px] xl:h-[300px]">
         <h2
-          class="text-[#e4dad1] sm:mt-14 sm:leading-[35px] lg:leading-[58px] xl:leading-[60px] sm:w-[200px] xl:w-[400px] font-bold font-['Bebas_Neue'] sm:text-[64px] lg:text-[128px]"
+          class="text-[#e4dad1] mt-14 sm:mt-14 leading-[30px] sm:leading-[35px] lg:leading-[58px] xl:leading-[60px] w-[150px] sm:w-[200px] xl:w-[400px] font-bold font-['Bebas_Neue'] text-[64px] sm:text-[64px] lg:text-[128px]"
         >
           Rust+
-          <span class="text-[#bab1a8] font-[300] sm:text-[32px] lg:text-[64px]"
+          <span
+            class="text-[#bab1a8] font-[300] text-[32px] sm:text-[32px] lg:text-[64px]"
             >Companion</span
           >
         </h2>
         <p
-          class="font-['Open_Sans'] xl:leading-[30px] sm:mt-4 xl:mt-4 text-base text-[#bab1a8] font-medium sm:w-full lg:w-[500px] xl:w-[550px]"
+          class="font-['Open_Sans'] xl:leading-[30px] mt-5 sm:mt-4 xl:mt-4 text-base text-[#bab1a8] font-medium w-10/12 sm:w-full lg:w-[500px] xl:w-[550px]"
         >
           The official Rust companion app – stay connected to the game so you
           never miss an important moment again. Coordinate with your team,
@@ -25,7 +26,7 @@
         </p>
         <a
           href="https://rust.facepunch.com/companion"
-          class="text-xl text-center flex items-center justify-center sm:py-1.5 lg:py-1 sm:w-[100px] lg:w-[110px] sm:mt-4 xl:mt-6 text-[#ffffff] font-['Bebas_Neue'] bg-[#cd412b]"
+          class="text-xl text-center flex items-center justify-center py-1 sm:py-1.5 lg:py-1 w-[110px] sm:w-[100px] lg:w-[110px] mt-5 sm:mt-4 xl:mt-6 text-[#ffffff] font-['Bebas_Neue'] bg-[#cd412b]"
           >learn more</a
         >
       </div>
