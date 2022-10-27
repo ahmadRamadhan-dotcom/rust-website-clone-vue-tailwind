c29c052fbc35bdc44ce8fbe09f832343c95c30fb
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-27 08:45:53 +0700

    chore: responsive header and banner hero for mobile devices

--- src/components/BannerHero.vue
+++ src/components/BannerHero.vue
@@ -25,30 +25,30 @@
       </video>
     </div>
     <div
-      class="absolute sm:top-24 md:top-60 lg:top-24 sm:left-10 lg:left-0 xl:left-32 2xl:left-0 2xl:right-0 2xl:bottom-0 2xl:top-40 2xl:w-[1100px] 2xl:m-auto"
+      class="wrapper absolute sm:top-24 md:top-60 lg:top-24 sm:left-10 lg:left-0 xl:left-32 2xl:left-0 2xl:right-0 2xl:bottom-0 2xl:top-40 2xl:w-[1100px] 2xl:m-auto"
     >
       <h1
-        class="font-['Bebas_Neue'] sm:w-72 xl:w-72 sm:leading-[80px] lg:leading-[160px] sm:text-[96px] lg:text-[192px] text-[#e4dad1] font-medium"
+        class="font-['Bebas_Neue'] sm:w-72 xl:w-72 leading-[80px] sm:leading-[80px] lg:leading-[160px] text-[96px] sm:text-[96px] lg:text-[192px] text-[#e4dad1] font-medium"
       >
         Explore Build Survive
       </h1>
       <p
-        class="text-base text-[#e4dad1] xl:mt-5 font-normal font-['Open_Sans'] sm:mb-5 lg:mb-7 xl:mb-8 sm:w-[96%] lg:w-6/12 xl:w-[550px]"
+        class="text-base text-[#e4dad1] mt-5 sm:mt-0 xl:mt-5 font-normal font-['Open_Sans'] mb-7 sm:mb-5 lg:mb-7 xl:mb-8 sm:w-[96%] lg:w-6/12 xl:w-[550px]"
       >
         The only aim in Rust is to survive. Everything wants you to die - the
         island’s wildlife and other inhabitants, the environment, other
         survivors. Do whatever it takes to last another night.
       </p>
-      <div class="sm:flex sm:gap-3 xl:gap-4">
+      <div class="flex sm:flex gap-3 sm:gap-3 xl:gap-4">
         <a
           href="https://rust.facepunch.com/buy"
           target="_blank"
-          class="sm:py-1.5 xl:py-1 sm:px-4 xl:px-5 text-[#ffffff] bg-[#cd412b] sm:text-[24px] lg:text-[28px] font-normal font-['Bebas_Neue']"
+          class="py-1.5 px-4 sm:py-1.5 xl:py-1 sm:px-4 xl:px-5 text-[#ffffff] bg-[#cd412b] text-[24px] sm:text-[24px] lg:text-[28px] font-normal font-['Bebas_Neue']"
           >buy rust</a
         >
         <a
           href="https://www.youtube.com/watch?v=LGcECozNXEw"
-          class="bg-[#2a2928] sm:px-5 xl:px-4 sm:py-1 xl:py-1 border text-[#e4dad1] sm:text-[24px] lg:text-[28px] font-normal font-['Bebas_Neue']"
+          class="bg-[#2a2928] px-5 sm:px-5 xl:px-4 py-1 sm:py-1 xl:py-1 border text-[#e4dad1] text-[24px] sm:text-[24px] lg:text-[28px] font-normal font-['Bebas_Neue']"
           target="_blank"
           >watch trailer</a
         >
@@ -75,3 +75,21 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+@media (max-width: 639px) {
+  #myVideo {
+    width: full;
+    height: 90vh;
+    object-fit: cover;
+  }
+  .wrapper {
+    top: 230px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    width: 83.333333%;
+  }
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="flex justify-center bg-[#151413] z-[999] md:translate-y-[140px] lg:translate-y-0 2xl:translate-y-[70px] sm:h-[27vh] md:h-0 lg:h-[9vh] xl:h-[11vh] 2xl:h-0"
+    class="flex justify-center bg-[#151413] z-[999] md:translate-y-[140px] lg:translate-y-0 2xl:translate-y-[70px] h-0 sm:h-[27vh] md:h-0 lg:h-[9vh] xl:h-[11vh] 2xl:h-0"
   >
     <div
       class="lg:w-[950px] xl:w-[1100px] sm:pb-1 md:pb-0 sm:grid z-[30] sm:place-content-end sm:place-items-center lg:flex lg:justify-between sm:items-end"
@@ -9,14 +9,15 @@
         <img
           src="../assets/rust-marque.svg"
           alt="rust header logo"
-          class="sm:-translate-y-6 lg:translate-y-1 xl:translate-y-2"
+          class="translate-y-8 translate-x-32 sm:translate-x-0 sm:-translate-y-6 lg:translate-y-1 xl:translate-y-2"
         />
       </figure>
       <div
-        class="sm:flex sm:gap-6 lg:gap-7 xl:gap-9 sm:translate-y-5 lg:translate-y-0"
+        class="flex gap-5 sm:flex sm:gap-6 lg:gap-7 xl:gap-9 sm:translate-y-5 lg:translate-y-0"
       >
         <p
-          class="text-[#e4dad1] font-['Bebas_Neue'] font-medium sm:text-xl lg:translate-y-3 xl:translate-y-3"
+          id="content"
+          class="text-[#e4dad1] translate-y-[90px] sm:translate-y-0 font-['Bebas_Neue'] font-medium text-xl sm:text-xl lg:translate-y-3 xl:translate-y-3"
           v-for="(data, index) in headerList"
           :key="index"
         >
@@ -25,7 +26,7 @@
         <a
           href="https://rust.facepunch.com/buy/"
           target="_blank"
-          class="sm:-translate-y-1 lg:translate-y-2 xl:translate-y-1 sm:py-1.5 sm:px-2 xl:py-1.5 xl:px-3 bg-[#cd412b] font-['Bebas_Neue'] font-medium sm:text-xl"
+          class="translate-y-20 sm:-translate-y-1 lg:translate-y-2 xl:translate-y-1 py-1.5 px-2 sm:py-1.5 sm:px-2 xl:py-1.5 xl:px-3 bg-[#cd412b] font-['Bebas_Neue'] font-medium text-xl sm:text-xl"
         >
           <p class="text-[#e4dad1]">buy rust</p>
         </a>
@@ -41,3 +42,5 @@ const headerList = [
   { id: 3, name: "official skins" },
 ];
 </script>
+
+<style scoped></style>
